e33f169a02ee94cc263309d6e55eeee939bc4618
Hashmat Ali <shahhashmatali667@gmail.com>
2022-03-18 00:11:46 +0500

    udpupdate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-import BasePage from './BasePage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AddPost from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./styles.css";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <BasePage/>
-    </>
+    <div className="App">
+      <ToastContainer />
+      <Navbar />
+      <Route exact path="/" component={() => <Home />} />
+      <Route exact path="/add" component={() => <AddPost />} />
+      <Route exact path="/edit/:id" component={() => <EditContact />} />
+    </div>
   );
-}
-
+};
 export default App;
